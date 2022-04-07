14104373c307b26f8a231ba255fe3af0ac8ceff6
KarlaDSJ <karla_dsj@ciencias.unam.mx>
2022-04-07 10:34:53 -0500

    Comentarios y algunos datos

--- README.md
+++ README.md
@@ -9,6 +9,22 @@ Se intentará replicar la página [*PlayPhrase*](https://www.playphrase.me/#/sea
 ## Corpus
 Creamos un programa en node que nos permite extraer los subtítulos de vídeos de Youtube junto con el tiempo en el que aparecen.
 
+Los canales utilizados para esta tarea son:
+
+|           id            |    canal      |
+|-------------------------|---------------|
+|UCshVTOdmZLdLj8LTV1j_0uw | tedespañol    |
+|UCZcvCpFcLxOKGbMocVgLjEA | kurzgesagt    |
+|UC3uPK6zOTe0HOfcIkuzII0Q | disneyplusla  |
+|UCBSs9x2KzSLhyyA9IKyt4YA | netflixanime  |
+|UCrVhY_d0L0qayRhMsRlPBOA | 31minutostv   |
+|UCJQQVLyM6wtPleV4wFBK06g | visualpolitik |
+|UC_Zc2fmbDpu_arkwvCDcX5g | cocina        |
+|UCyQqzYXQBUWgBTn4pw_fFSQ | auronplay     |
+|UCX6b17PVsYBQ0ip5gyeme   | crash course  |
+
+Utilizamos estos canales porque hablan de temas muy variados
+
 ### Instalación 
 ```
 cd corpus
--- corpus/index.js
+++ corpus/index.js
@@ -10,17 +10,25 @@ chanelsDavid = [ "UCX6b17PVsYBQ0ip5gyeme", "UC3uPK6zOTe0HOfcIkuzII0Q",
 
 /**
  * Escribe en un archivo la información de los videos de un canal de youtube 
- * @param {string} chanel 
- * @param {json} json 
+ * @param {string} chanel id del canal
+ * @param {json} json id, título, subtítulos de cada video del canal 
  */
 function write(chanel, json){
   try {
+    //pasamos el json a string pero en conservando la identación
     fs.writeFileSync(chanel,JSON.stringify(json, null,2))
   } catch (error) {
     console.log("Algo salió mal "+ error);
   }
 }
 
+/**
+ * Dado id de cada video en el canal obtiene los subtítulos en español, 
+ * en caso de no poder obtener los subtítulos del vídeo este se elimina 
+ * del json dado
+ * @param {json} videos id, título de cada video del canal 
+ * @returns regresa el json con los subtítulos de los videos
+ */
 async function getAllSubtitles(videos){
   fails = []
   for (video of videos) {
@@ -30,14 +38,23 @@ async function getAllSubtitles(videos){
         lang: "es", // default: `en`
       });
     } catch (error) {
+      //En caso de no obtener los subtítulos
       fails.push(video.id)
     }
   }
+  //Quitamos los vídeos sin subtítulos
   return videos.filter((x) => !fails.includes(x.id))
 }
 
+/**
+ * Obtiene el id y nombre de los vídeos de un canal de youtube y
+ * posteriormente los subtítulos de cada uno, guarda esta información
+ * en un archivo .json
+ * @param {string} chanel id del canal
+ */
 async function scraper(chanel) {
   videos = await getChannelVideos(chanel);
+  //Quitamos datos innecesarios
   videos = videos.map((x) => {
     delete x.duration
     delete x.artist
@@ -45,10 +62,13 @@ async function scraper(chanel) {
     delete x.publishedAt
     return x
   });
+  //obtenemos los subtítulos
   videos = await getAllSubtitles(videos)
+  //guardamos
   write("data/"+chanel+".json", videos)
 }
 
-for (const id in object) {
+//obtenemos los subtítulos de los videos de varios canales
+for (const id of chanelsKarla) {
   scraper(id)
 }
