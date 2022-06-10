cd8cdc2e7e486dde7e48dc8583aaf10d8af5be4f
KarlaDSJ <karla_dsj@ciencias.unam.mx>
2022-06-10 04:07:35 -0500

    Agregamos comentarios al readme, cambios al evaluar clusters, se propone otro método para RI

--- src/utils.py
+++ src/utils.py
@@ -11,13 +11,13 @@ import re #regex
 more = ["si", "bien", "ahora", "así", "aquí", "pues"]
 stopwords_list = stopwords.words('spanish') + more
 
-def get_tokens_clean(text):
+def get_tokens_clean(text, stop_words=False):
     '''
     Genera los tokens de una cadena y los limpia
     (quita símbolos raros y stopwords)
-    
+        
     Args:
-        text (str): cadena
+            text (str): cadena
     '''
     tokens = word_tokenize(text)
     clean = []
@@ -25,7 +25,9 @@ def get_tokens_clean(text):
     for w in tokens:
         #quita stopwords y convierte a minúsculas
         w = re.sub(pattern,'', w.lower())
-        if w != '':
+        if w not in stopwords_list and w != '':
+            clean.append(w)
+        elif not stop_words and w != '':
             clean.append(w)
     return clean
 
